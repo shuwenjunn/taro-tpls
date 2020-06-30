30ae79a2a5ee5e5055fee3c5e44faa60410fc3c9
shuwenjun <shuwenjun77@gmail.com>
2020-06-30 09:37:04 +0800

    调整账单页面

--- src/tpls/bill/pages/billDetail/index.tsx
+++ src/tpls/bill/pages/billDetail/index.tsx
@@ -1,6 +1,8 @@
 import Taro, {Component, Config} from '@tarojs/taro'
-import {View} from '@tarojs/components'
+import {View, Image} from '@tarojs/components'
 import "taro-ui/dist/style/components/float-layout.scss"
+import checkIcon from '../../assets/images/check.svg'
+import shenglueIcon from '../../assets/images/shenglue.svg'
 import styles from './style.module.less'
 import {config} from '../../config'
 
@@ -12,7 +14,9 @@ type PageDispatchProps = {}
 
 type PageOwnProps = {}
 
-type PageState = {}
+type PageState = {
+  type: string
+}
 
 type IProps = PageStateProps & PageDispatchProps & PageOwnProps
 
@@ -22,11 +26,15 @@ export default class Index extends Component<IProps, PageState> {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      type: ''
+    }
   }
 
   componentDidMount() {
-
+    this.setState({
+      type: this.$router.params.type
+    })
   }
 
   config: Config = {
@@ -37,6 +45,7 @@ export default class Index extends Component<IProps, PageState> {
   }
 
   componentDidHide() {
+
   }
 
 
@@ -53,12 +62,16 @@ export default class Index extends Component<IProps, PageState> {
       },
     ]
 
+    const {type} = this.state
+    console.log('type', type)
     return (
       <View className={styles.index}>
         <View className={styles.top}>
-          <View className={styles.type}>银行卡充值</View>
-          <View className={styles.money}>100.00</View>
-          <View className={styles.status}>交易成功</View>
+          <View className={styles.type}>{type === 'income' ? '银行卡充值' : '提现到银行卡'}</View>
+          <View className={styles.money}>{type === 'output' ? '-' : ''}100.00</View>
+          <View className={type === 'income' ? `${styles.status}` : `${styles.status} ${styles.progress}`}>
+            {type === 'income' ? '交易成功' : '处理中'}
+          </View>
         </View>
 
         <View className={styles.info}>
@@ -69,7 +82,9 @@ export default class Index extends Component<IProps, PageState> {
                 <View className={styles.logisticsIt} key={index}>
                   {index < logisticsData.length - 1 &&
                   <View className={index + 1 <= 1 ? styles.line : `${styles.line} ${styles.gray}`}></View>}
-                  <View className={index <= 1 ? styles.dot : `${styles.dot} ${styles.gray}`}></View>
+                  <View className={index <= 1 ? styles.dot : `${styles.dot} ${styles.gray}`}>
+                    {index <= 1 ? <Image src={checkIcon} /> : <Image src={shenglueIcon} />}
+                  </View>
                   <View className={index <= 1 ? styles.content : `${styles.content} ${styles.gray}`}>
                     {item.context}
                   </View>
@@ -90,8 +105,6 @@ export default class Index extends Component<IProps, PageState> {
             <View className={styles.value}>招商银行（9809）李盼盼</View>
           </View>
         </View>
-
-
       </View>
     )
   }
